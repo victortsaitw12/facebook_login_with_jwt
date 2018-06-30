9642a5aed5b08940d70e39cd639ad586857acc5f
victor <cooldan.tw@gmail.com>
2018-06-30 14:44:06 +0800

    third commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'facebook': {
     'appID': '843930asfdasfd9103568',
-    'appSecret': '75daf458e42aadfasdf0048d16055a1dab',
+    'appSecret': '975daf458e42aadfasdf0048d16055a1dab',
     'callbackURL': 'http://localhost:8080/auth/facebook/callback',
     'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name, last_name, email',
     'profileFields': ['id', 'email', 'name']
